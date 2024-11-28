af99cdd7b3cdeef6bd9a2b3bbd11f280063a8d2c
kolozsvari <106528317+kolozsvari@users.noreply.github.com>
2024-11-28 10:32:29 +0100

    names.blade.php

--- resources/views/pages/names.blade.php
+++ resources/views/pages/names.blade.php
@@ -9,6 +9,7 @@
             <tr>
                 <th>Azonosító</th>
                 <th>Név</th>
+                <th>Családnév</th>
                 <th>Létrehozás</th>
             </tr>
         </thead>
@@ -16,12 +17,19 @@
             @foreach($names as $name)
                 <tr>
                     <td>{{ $name->id }}</td>
+                    @empty($name->family)
+                        <td><strong>Nincs adat.</strong></td>
+                    @else
+                        <td>{{ $name->family->surname }}</td>
+                    @endempty
                     <td>{{ $name->name }}</td>
+
                     <td>{{ $name->created_at }}</td>
                 </tr>
             @endforeach
         </tbody>
     </table>
 
+   
 </div>
 @endsection
